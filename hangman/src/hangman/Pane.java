04e9a1f58ad1d3cb25c9d47ab373ecbde1e29171
Rose <nowakarose@gmail.com>
2017-12-11 22:37:49 -0600

    Done :)

--- hangman/src/hangman/Pane.java
+++ hangman/src/hangman/Pane.java
@@ -1,342 +1,217 @@
 package hangman;
 
-import java.awt.*;
-import java.awt.event.*;
-import java.util.ArrayList;
 import javax.swing.*;
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.geom.Line2D;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 
+public class Pane extends JFrame {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1153943849832065878L;
 
-import javax.swing.JPanel;
-/** class to display a JFrame object using border layout */
-
-
-public class Pane extends JFrame implements ActionListener {
 	public static void main(String[] args) {
-		Pane a = new Pane();
-		a.setVisible(true);
-	}
- private JPanel centerPanel;
- private JPanel southPanel;
- private JTextField textField;
- private LinePanel line;
- public static String word="";
- public static String genre;
- public static String[][] answers = { 	 {"tomato", "apple", "kiwi", "orange", "asparagus", "radish", "carrot", "banana", "peach", "zucchini"},
-	                              	     {"dog", "kangaroo", "elephant", "giraffe", "panda", "crocodile", "hamster", "lemur", "jaguar", "aardvark"},
-	                                     {"illinois", "michigan", "mississippi", "montana", "kentucky", "arkansas", "alaska", "indiana", "nebraska", "california"} };
+        new Pane();
+    }
+    public static String word = "";
+    public static String genre = "";
+    public static String[][] answers= {
+    	 {"tomato", "apple", "kiwi", "orange", "asparagus", "radish", "carrot", "banana", "peach", "zucchini"},
+ 	     {"dog", "kangaroo", "elephant", "giraffe", "panda", "crocodile", "hamster", "lemur", "jaguar", "aardvark"},
+         {"illinois", "michigan", "mississippi", "montana", "kentucky", "arkansas", "alaska", "indiana", "nebraska", "california"} };
+   
+    private JLabel commandTitle = new JLabel("Press 'Enter' to continue");
+    private JLabel wordtoUse = new JLabel(word);
+    private JLabel wGuesses = new JLabel();
+    private JTextField textBox = new JTextField(20);
+    private BufferedImage hangman = new BufferedImage(500, 500, BufferedImage.TYPE_INT_ARGB);
+    private JPanel hangmanPanel;
+    private JPanel inputPanel;
+    private String wordToGuess = null;
+    private int numWrong= 0;
+    private String wrongGuessesS = "Wrong Guesses: ";
+    private String Knownword = "";
 
- public ArrayList<String> usedLetters = new ArrayList(); // list of used letter by user
- public ArrayList<String> correctLetters = new ArrayList();
- public String userInput = "";
+    public Pane() {
+    	pickWord(answers);
+        setLayout(new BorderLayout());
+        drawHangmanStand(hangman);
 
- private int numLives = 6; // number of lives
  
- JButton exitButton;
- JButton playAgain;
+        inputPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.ipadx = 1;
+        gridBagConstraints.gridy = 1;
+        inputPanel.add(commandTitle, gridBagConstraints);
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 1;
+        inputPanel.add(textBox, gridBagConstraints);
 
- // no-argument constructor
- public Pane() { 
-  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  pickWord(answers);
-  correctLetters = new ArrayList(word.length());
+        add(inputPanel);
+
+        textBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	
+                if (wordToGuess == null) {
+                    wordToGuess = word.toLowerCase();
+             
+                    textBox.setText("");
+                    
+                commandTitle.setText("Guess a letter");
+                    String displayWord = "";
+                    for (int i = 0; i < wordToGuess.length(); i++) {
+                        Knownword += "_";
+                        displayWord += " _ ";
+                    }
+                    wordtoUse = new JLabel(displayWord);
+                    GridBagConstraints gridBagConstraints1 = new GridBagConstraints();
+                    gridBagConstraints1.gridx = 1;
+                    gridBagConstraints1.gridy = 2;
+                    gridBagConstraints1.gridwidth = 2;
+                    inputPanel.add(wordtoUse, gridBagConstraints1);
+                    gridBagConstraints1 = new GridBagConstraints();
+                    gridBagConstraints1.gridx = 1;
+                    gridBagConstraints1.gridy = 3;
+                    gridBagConstraints1.gridwidth = 2;
+                    inputPanel.add(wGuesses, gridBagConstraints1);
+
+                    return;
+                }
+
+                if (wordToGuess.indexOf(textBox.getText()) >= 0) {
+                    guessRight();
+                } else {
+                    guessWrong();
+                    textBox.setText("");
+                }
+
+
+            }
+        });
+
+        setSize(1200, 600);
+        setVisible(true);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        showChangedHangman();
+        revalidate();
+
+    }
+    
+    private static void pickWord(String [][] a){
+   	 int column = (int) (Math.random() * 3)  ;
+   		int row = (int)(Math.random()* 9);
+   		word = a[column][row];
+   		word = word.toLowerCase();
+   		if(column==0) {
+   			genre = "Fruits/Vegetables";
+   		}
+   		else if(column==1) {
+   			genre = "Animals";
+   		} 
+   		else {
+   			genre = "States";
+   		}
+   	}
+
   
-  
+    private void guessWrong() {
+        numWrong++;
+        wrongGuessesS += textBox.getText() + ", ";
+        wGuesses.setText(wrongGuessesS);
+        Graphics2D g = (Graphics2D) hangman.getGraphics();
+        int x = 250, y = 200;
+        g.setStroke(new BasicStroke(3));
+        g.setColor(Color.ORANGE);
 
-  setSize(600,500);
-  setLocation(100, 100);
-  setTitle("Hangman Game");
-  setLayout(new BorderLayout());
-
-  centerPanel = new JPanel();
-  line = new LinePanel(15,word,usedLetters);
-  centerPanel.setSize(500,500);
-  centerPanel.setBackground(Color.WHITE);
-  centerPanel.add(line);
-  add(centerPanel, BorderLayout.CENTER);
-
-  textField = new JTextField(20);
-  textField.addActionListener(this);
-
-  playAgain = new JButton("Play Again");
-  exitButton = new JButton("Exit");
-  exitButton.addActionListener(this);
-  playAgain.addActionListener(this);
-
-  southPanel = new JPanel();
-  southPanel.setBackground(Color.BLACK);
-  southPanel.setLayout(new  GridLayout(0,3));
-  southPanel.add(playAgain);
-  southPanel.add(textField);
-  southPanel.add(exitButton);
-  add(southPanel, BorderLayout.SOUTH);
- 
-
- }
-
- // Picks a word, latter it will be picked randomly.
- private static void pickWord(String [][] a){
-	 int column = (int) (Math.random() * 3)  ;
-		int row = (int)(Math.random()* 9);
-		word = a[column][row];
-		word = word.toLowerCase();
-		if(column==0) {
-			genre = "Fruits/Vegetables";
-		}
-		else if(column==1) {
-			genre = "Animals";
-		} 
-		else {
-			genre = "States";
-		}
-	}
- 
-
- // This method check wither the input is valid
- // i.e. its in the alphabet.
- private boolean checkInput(String s){
-  String [] alphabet = {"a","b","c","d","e","f",
-    "g","h","i","j","k","l","m","n","o","p",
-    "q","r","s","t","u","v","w","x","y","z"};
-
-  for (int i = 0; i < alphabet.length; i++){
-   if ((s.equalsIgnoreCase(alphabet[i])) && s.length() <= 1){
-    return true;
-   }
-  }
-  return false;
- }
- 
- /**
-  * 
-  */
- public void Play(){
-
-
-  if (numLives > 0){
-
-   if (userInput.length() == 1 && usedLetters.contains(userInput) == false &&
-     checkInput(userInput) == true){
-    usedLetters.add(userInput);
-
-    if (word.contains(userInput) == true){
-     correctLetters.add(userInput);
-     centerPanel.removeAll();
-
-     line = new LinePanel(20,word,correctLetters);
-     centerPanel.add(line);
-     centerPanel.revalidate();
+        
+        if (numWrong  == 1){
+        	 g.drawOval(-20 + x, y, 40, 40);
+        }
+        else if (numWrong == 2){
+        	 g.drawLine(x, y + 40, x, y + 40 + 80);
+        }
+        else if (numWrong == 3) {
+        	 g.drawLine(x, y + 40 + 20, x + 20, y + 40 + 60);
+        }
+        else if (numWrong == 4) {
+        	g.drawLine(x, y + 40 + 20, x - 20, y + 40 + 60);
+        }
+        else if (numWrong == 5) {
+        	g.drawLine(x, y + 40 + 80, x + 20, y + 40 + 80 + 40);
+        }
+        else if (numWrong == 6) {
+        	g.drawLine(x, y + 40 + 80, x - 20, y + 40 + 80 + 40);
+        }
+        
+        else {
+        	 JOptionPane.showMessageDialog(this, "You Lose!");
+        }
+        
+        g.dispose();
+        showChangedHangman();
+        revalidate();
     }
 
-    else{
-     numLives = numLives - 1;
+    private void showChangedHangman() {
+        if (hangmanPanel != null)
+            remove(hangmanPanel);
 
-     centerPanel.removeAll();
-     line = new LinePanel(numLives,word,correctLetters);
+        hangmanPanel = new JPanel();
+        hangmanPanel.add(new JLabel(new ImageIcon(hangman)));
+        add(hangmanPanel, BorderLayout.EAST);
+        revalidate();
 
-     centerPanel.add(line);
-     centerPanel.revalidate();
-    }
-   }
-
-   else if (userInput.length() > 1)
-    System.out.println("Enter a valid letter");
-
-   else if (userInput.length() == 1 && checkInput(userInput) == true && word.contains(userInput)){
-    correctLetters.add(userInput);
-   }
-
-   centerPanel.removeAll();
-
-   line = new LinePanel(20,word,usedLetters);
-   centerPanel.add(line);
-   centerPanel.revalidate();
-  }  
-}
- // return true if the word and the correctly used letters list match
- public boolean checkWord(String s, ArrayList<String> t){
-  String temp = "";
-
-  for (int i = 0; i < s.length(); i++){
-   if ( t.contains(s.substring(i, i+1)) == true){
-    temp += s.substring(i, i+1);
-   }
-  }
-
-  if (s.equals(temp)){
-   return true;
-  }
-
-  return false;
- }
-
- public void actionPerformed(ActionEvent evt) {
-  String temp = textField.getText();
-  if (temp.length() == 1){
-   userInput = temp;
-  }
-  textField.selectAll();
-  if (checkWord(word, correctLetters) != true){
-   Play();
-  }
-
-  if (evt.getSource() == exitButton){
-   System.exit(0);
-  }
-
-//  if (evt.getSource() == playAgain){
-//   removeAll();
-//   repaint();
-//   
-//  }
-
- }
-
-
-
- class LinePanel extends JPanel {
-
- int x = 5;
- String theWord = "";
- ArrayList<String> letterList;
-
- public LinePanel(int num, String t, ArrayList<String> s) {
-  super(); // 300,350
-  setPreferredSize(new Dimension(400,400));
-  setBackground(Color.RED);
-  this.x = num;
-  this.theWord = t;
-  letterList = cloneList(s);
- }
-
- private ArrayList<String> cloneList(ArrayList<String> aList) {
-  ArrayList<String> clonedList = new ArrayList<String>(aList.size());
-  for (String letter : aList) 
-   clonedList.add(letter);
-  return clonedList;
- }
-
-
-
- public int getX() {
-  return x;
- }
-
- public void setX(int x) {
-  this.x = x;
- }
-
- public void paintComponent(Graphics g) {
-  super.paintComponent(g);
-  Graphics2D g2 = (Graphics2D) g;
-  if (x == 15 || x != 15){
-   Line2D line = new Line2D.Double(0, 250, 80, 250); // Creates base
-   Line2D line2 = new Line2D.Double(40, 50, 40, 250); // Creates vertical line
-   Line2D line3 = new Line2D.Double(40, 50, 150, 50); // Creates horizontal line
-   Line2D line4 = new Line2D.Double(150, 50, 150, 80); // Creates small line to hang the man
-   g2.setStroke(new BasicStroke(5.0f)); // Line thickness
-   g2.setColor(Color.BLACK); // Line colour
-
-   //draw shape of line
-   g2.draw(line);
-   g2.draw(line2);
-   g2.draw(line3);
-   g2.draw(line4);
-
-   int x1 = 0; int y = 320;
-   for (int i = 0; i < theWord.length();i++){
-    g2.drawLine(x1, y, x1 + 20, y);
-    x1 += 50;
-   }
-  }
-
-  // head
-  if (x == 5 || x < 5){
-   g2.setStroke(new BasicStroke(5.0f));
-   g2.drawOval(135, 85, 35, 35);
-  }
-
-  // body
-  if (x == 4 || x < 4){
-   g2.drawLine(150, 120, 150, 190);
-  }
-
-  // left arm
-  if (x == 3 || x < 3){
-   g2.drawLine(150, 140, 125, 155); 
-  }
-
-  // right arm
-  if (x == 2 || x < 2){
-   g2.drawLine(150, 140, 175, 155); 
-  }
-
-  // left leg and foot
-  if (x == 1 || x < 1){
-   g2.drawLine(150, 190, 125, 200); // leg
-   g2.drawLine(125, 200, 120, 190); // foot
-  }
-
-  // right leg and foot
-  if (x == 0){
-   g2.drawLine(150, 190, 175, 200); // leg
-   g2.drawLine(175, 200, 180, 190); // foot
-  }
-
-  // Show whole word on screen
-  if (x == 20){
-
-   int x1 = 3; int y = 317;
-
-   String temp = "";
-
-   for (int i = 0; i < theWord.length(); i++){
-    if ( letterList.contains(theWord.substring(i, i+1)) == true){
-     temp += theWord.substring(i, i+1);
-    }
-    else{
-     temp += " ";
-    }
-   }
-
-   for (int i = 0; i < temp.length() ;i++){
-    g2.setColor(Color.BLACK);
-    Font font = new Font("Arial", Font.PLAIN, 25);
-    g2.setFont(font);
-    g2.drawString(temp.substring(i, i+1), x1, y);
-    x1 += 50;
-   }
-
-
-   x1 = 3;  y = 370;
-   for (int i = 0; i < letterList.size() ;i++){
-
-    if (theWord.contains(letterList.get(i)) == false){
-     g2.drawString(letterList.get(i), x1, y );
-     x1 += 50;
     }
 
-   }
-  }
- }
-}
+    private static void drawHangmanStand(BufferedImage image) {
+        Graphics2D g = (Graphics2D) image.getGraphics();
+        g.setStroke(new BasicStroke(3));
+        g.setColor(Color.BLUE);
 
 
+        g.drawLine(10, 475, 250, 475);
+        g.drawLine(100, 475, 100, 100);
+        g.drawLine(100, 100, 250, 100);
+        g.drawLine(250, 100, 250, 200);
 
-class PlayHangman {
+        g.dispose();
+    }
+    
+    private void guessRight() {
+        String guess = textBox.getText().toLowerCase();
+        addGuessToKnownWord(guess);
+        String displayString = "";
+        for (int i = 0; i < Knownword.length(); i++) {
+            displayString += Knownword.substring(i, i + 1) + " ";
+        }
+        wordtoUse.setText(displayString);
 
- /**
-  * @param args
-  */
-}
+        if (Knownword.indexOf("_") < 0) {
+            JOptionPane.showMessageDialog(this, "You Win!");
+        }
+        textBox.setText("");
+    }
+
+    private void addGuessToKnownWord(String guess) {
+        ArrayList<Integer> indexes = new ArrayList<>();
+        for (int index = wordToGuess.indexOf(guess);
+             index >= 0;
+             index = wordToGuess.indexOf(guess, index + 1)) {
+            indexes.add(index);
+        }
+        for (int i = 0; i < indexes.size(); i++) {
+            int index = indexes.get(i);
+            StringBuilder stringBuilder = new StringBuilder(Knownword);
+            stringBuilder.replace(index, index + guess.length(), guess);
+            Knownword = stringBuilder.toString();
+        }
+    }
 }
